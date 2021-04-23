dbf57484188310be8415e3f6114e480318124f9b
Jake Chadwell <jake.chadwell01@gmail.com>
2021-04-23 11:35:27 -0500

    java syntax, types, and variables exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,48 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int myFavoriteNumber = 5;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hey there!";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+////        x = x + 5;
+//        x+=5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y*=x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+////        x = x / y;
+////        y = y - x;
+//        x/=y;
+//        y-=x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+//        int newInt = Integer.MAX_VALUE;
+//        int newInt = 2147483648;
+//        System.out.println(newInt);
     }
 }
