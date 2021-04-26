5d61599a065c4e87f11d720b6d11e278804a1598
Jake Chadwell <jake.chadwell01@gmail.com>
2021-04-26 17:58:51 -0500

    cleaned up code

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
+//
+//      First Section:
+//
+//        Number 1:
+//
 //        double pi = 3.14159;
 //        System.out.printf("The value of pi is approximately %s.\n", pi);
 //
+//      Second Section:
+//
+//        Number 1:
+
         Scanner scanner = new Scanner(System.in);
 //        System.out.println("Enter an integer: ");
 //        int userInput = scanner.nextInt();
 //        System.out.printf("You entered: %d%n", userInput);
 //
+//        Number 2:
+
 //        System.out.println("Enter three words: ");
 //        String first = scanner.next();
 //        String second = scanner.next();
@@ -17,9 +28,13 @@ public class ConsoleExercises {
 //        System.out.printf("Second word is: %s%n", second);
 //        System.out.printf("Third word is: %s%n", third);
 
+//        Number 3/4:
+
 //        System.out.println("Enter a sentence: ");
 //        String userSentence = scanner.nextLine();
 //        System.out.printf("Your sentence: %s%n", userSentence);
+
+//        Bonus:
 
         System.out.println("Enter classroom dimensions (L,W): ");
         float length = scanner.nextFloat();
