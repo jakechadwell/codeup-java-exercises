9a7fce481a5aad37fd9649d7868cfe8a706b826a
Jake Chadwell <jake.chadwell01@gmail.com>
2021-05-04 11:40:46 -0500

    Completed bonus for arrays exercise

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,5 +1,8 @@
 package movies;
 import util.Input;
+
+import java.util.Arrays;
+
 public class MoviesApplication {
     public static void main(String[] args) {
         Movie[] movies = MoviesArray.findAll();
@@ -7,7 +10,7 @@ public class MoviesApplication {
         while(true) {
             options();
             Input userInput = new Input();
-            int userChoice = userInput.getInt(0, 5, "What would you like to do?: ");
+            int userChoice = userInput.getInt(0, 6, "What would you like to do?: ");
             if (userChoice == 0){
                 break;
             } else if (userChoice == 1){
@@ -19,7 +22,10 @@ public class MoviesApplication {
             } else if (userChoice == 4){
                 returnListOfSpecifiedMovies(movies, "horror");
             } else if (userChoice == 5){
-                returnListOfSpecifiedMovies(movies, "scifi");
+                returnListOfSpecifiedMovies(movies, "sci-fi");
+            } else if (userChoice == 6){
+                movies = addNewMovie(movies, userMovie());
+                System.out.println("\nYour movie was successfully added!\n");
             }
         }
         System.out.println("See ya!");
@@ -33,6 +39,7 @@ public class MoviesApplication {
             System.out.println("3 - view movies in the drama category");
             System.out.println("4 - view movies in the horror category");
             System.out.println("5 - view movies in the sci-fi category");
+            System.out.println("6 - add a new movie");
     }
     public static void returnMovie(Movie movie){
         System.out.println(movie.getName() + " -- " + movie.getCategory());
@@ -55,5 +62,14 @@ public class MoviesApplication {
             }
         }
         System.out.println();
+    }
+    public static Movie[] addNewMovie(Movie[] moviesArray, Movie movie){
+        moviesArray = Arrays.copyOf(moviesArray, moviesArray.length+1);
+        moviesArray[moviesArray.length-1] = movie;
+        return moviesArray;
+    }
+    public static Movie userMovie(){
+        Input userInput = new Input();
+        return new Movie(userInput.getString("Enter a new movie name: "), userInput.getString("Enter the genre of your movie: "));
     }
 }
